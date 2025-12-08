527153fc3f01c06317386e965b2b9f786a8cb381
FlyingGrass <emre.bozqurt@gmail.com>
2025-12-08 21:27:59 +0300

    Update page.tsx

--- app/letters/page.tsx
+++ app/letters/page.tsx
@@ -34,22 +34,11 @@ const Letters = () => {
         <h1 className='text-white text-center text-5xl max-sm:text-3xl max-sm:pt-6 pt-12'>Letter From The <br className='sm:hidden' /> <span className='text-[hsl(42,72%,52%)] font-black'> Secretary General </span></h1>
 
         <p className='text-white mx-auto w-[1000px] max-sm:w-[350px] max-sm:text-sm text-2xl bg-black/60 relative rounded-4xl max-sm:rounded-2xl max-sm:px-8 max-sm:py-4 max-sm:my-6 px-16 py-8 my-12'>
-          Hello dear attendants, <br /> <br />
+          Dear Participants<br /> <br />
 
-          But I must explain to you how all this mistaken idea of denouncing
-          pleasure and praising pain was born and I will give you a complete
-          account of the system, and expound the actual teachings of the great
-          explorer of the truth, the master-builder of human happiness. No one
-          rejects, dislikes, or avoids pleasure itself, because it is pleasure, but
-          because those who do not know how to pursue pleasure rationally
-          encounter consequences that are extremely painful. Nor again is there
-          anyone who loves or pursues or desires to obtain pain of itself, because
-          it is pain, but because occasionally circumstances occur in which toil
-          and pain can procure him some great pleasure. To take a trivial example,
-          which of us ever undertakes laborious physical exercise, except to obtain
-          some advantage from it? But who has any right to find fault with a man
-          who chooses to enjoy a pleasure that has no annoying consequences, or
-          one who avoids a pain that produces no resultant pleasure?
+          It is a great pleasure and honor to invite you to the first  annual session of Bornova Model United Nations . I am excited and proud to serve as the Secretary General of this conference. We worked hard and prepared meticulously with my team to make BornovaMun'26 happen. The aim of this conference is to offer you a conference like no other and to increase the cultural and social event opportunities in our beautiful Bornova. This year's inaugural conference features 7 committees and will welcome over 350 participants to our beloved Bornova. We started working on this conference 6-7 months ago and managed to overcome the challenges we faced with my team. We worked with the same enthusiasm and passion throughout this process. I am excited and looking forward to seeing you at Teknokent College on January 16! If you have any questions about our conference, please feel free to contact me anytime. 
+          <br/><br/><span className="text-[hsl(42,72%,52%)] font-black">Cem Deniz</span>, Secretary General
+          Bornova Model United Nations 2026
 
           <Image
             src="/mail.svg"
